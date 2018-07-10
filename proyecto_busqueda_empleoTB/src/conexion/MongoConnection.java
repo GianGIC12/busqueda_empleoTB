11874427f49d2b1a003d557231187dc9cb3b0364
dtransforma <giancarlo.inga.c@gmail.com>
2018-07-10 16:58:37 -0500

    Iniciando_busqueda

---
+++ proyecto_busqueda_empleoTB/src/conexion/MongoConnection.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+/**
+ *
+ * @author user
+ */
+public class MongoConnection {
+
+    MongoClient mongoC;
+    String gestor, bd, user, pass;
+    DB dataBase = null;
+    DBCollection collection1 = null;
+
+    public MongoConnection() {
+
+        gestor = "mdb.todobusco.com";
+        bd = "mdb_todobusco_prod";
+        user = "u_todobusco_prod";
+        pass = "Shai7te5aesheu";
+
+    }
+
+    public void connectDataBase() {
+
+        MongoClientURI uri = new MongoClientURI("mongodb://" + user + ":" + pass + "@" + gestor + ":27017" + "/" + bd);
+        mongoC = new MongoClient(uri);
+        dataBase = mongoC.getDB(bd);
+
+    }
+
+    public void disconnectDataBase() {
+
+        mongoC.close();
+
+    }
+
+    public MongoClient getMongoC() {
+        return mongoC;
+    }
+
+    public void setMongoC(MongoClient mongoC) {
+        this.mongoC = mongoC;
+    }
+
+    public String getGestor() {
+        return gestor;
+    }
+
+    public void setGestor(String gestor) {
+        this.gestor = gestor;
+    }
+
+    public String getBd() {
+        return bd;
+    }
+
+    public void setBd(String bd) {
+        this.bd = bd;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public DB getDataBase() {
+        return dataBase;
+    }
+
+    public void setDataBase(DB dataBase) {
+        this.dataBase = dataBase;
+    }
+
+    public DBCollection getCollection1() {
+        return collection1;
+    }
+
+    public void setCollection1(DBCollection collection1) {
+        this.collection1 = collection1;
+    }
+    
+    
+    
+    
+
+}
